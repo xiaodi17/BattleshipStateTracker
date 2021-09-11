4b0f0f4531416dc6a1f081cc6e62aa071bbf98f0
David Lu <xiaodi.david@gmail.com>
2021-09-12 01:11:30 +1000

    add battleship controller

--- BattleshipStateTracker.API/Controllers/BattleshipController.cs
+++ BattleshipStateTracker.API/Controllers/BattleshipController.cs
@@ -2,9 +2,12 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using BattleshipStateTracker.Service;
+using BattleshipStateTracker.Service.Exceptions;
+using BattleshipStateTracker.Service.Models;
 
 namespace BattleshipStateTracker.API.Controllers
 {
@@ -23,12 +26,12 @@ namespace BattleshipStateTracker.API.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Create a new board
         /// </summary>
         /// <param name="boardId"></param>
         /// <param name="size"></param>
         /// <returns></returns>
-        [HttpGet("Create")]
+        [HttpPost("create")]
         public IActionResult Create(string boardId, int size = 10)
         {
             try
@@ -38,6 +41,30 @@ namespace BattleshipStateTracker.API.Controllers
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex.Message);
+                return StatusCode(500, ex.Message);
+            }
+        }
+        
+        [HttpPut("battleship")]
+        public async Task<IActionResult> AddBattleShip(string boardId, int startRow, int startCol, int endRow, int endCol)
+        {
+            try
+            {
+                var battleShip = await _battleshipService.AddBattleShip(
+                    boardId,
+                    new Point(startRow, startCol),
+                    new Point(endRow, endCol));
+                return Ok(battleShip);
+            }
+            catch (InvalidBattleshipCreateException ex)
+            {
+                _logger.LogError(ex.Message);
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
                 return StatusCode(500, ex.Message);
             }
         }
--- BattleshipStateTracker.API/Startup.cs
+++ BattleshipStateTracker.API/Startup.cs
@@ -8,9 +8,12 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using BattleshipStateTracker.Service;
+using Microsoft.OpenApi.Models;
 
 namespace BattleshipStateTracker.API
 {
@@ -26,13 +29,22 @@ namespace BattleshipStateTracker.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddSingleton<IBattleshipService, BattleshipService>();
+            services.AddCors();
+            services.AddHttpClient();
+            services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "BattleshipStateTracker");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
